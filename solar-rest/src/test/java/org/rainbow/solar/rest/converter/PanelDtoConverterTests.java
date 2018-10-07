f87fc1f2e4f9106ef94a70b9cabd1a5d7124b048
Paul Muriel Biya-Bi <biya-bi@hotmail.fr>
2018-10-07 13:49:37 -0400

    Follow test conventions in the PanelDtoConverterTests class
    One of our test conventions is to have expected values as first
    arguments and actual values as second arguments. This convention was
    not followed in the PanelDtoConverterTests class and has been
    corrected.

--- solar-rest/src/test/java/org/rainbow/solar/rest/converter/PanelDtoConverterTests.java
+++ solar-rest/src/test/java/org/rainbow/solar/rest/converter/PanelDtoConverterTests.java
@@ -67,11 +67,11 @@ public class PanelDtoConverterTests {
 
 		Panel panel = PanelDtoConverter.fromDto(panelDto);
 
-		Assert.assertEquals(panel.getSerial(), panelDto.getSerial());
-		Assert.assertEquals(panel.getLatitude(), panelDto.getLatitude());
-		Assert.assertEquals(panel.getLongitude(), panelDto.getLongitude());
-		Assert.assertEquals(panel.getBrand(), panelDto.getBrand());
-		Assert.assertEquals(panel.getUnitOfMeasure().toString(), panelDto.getUnitOfMeasure());
+		Assert.assertEquals(panelDto.getSerial(), panel.getSerial());
+		Assert.assertEquals(panelDto.getLatitude(), panel.getLatitude());
+		Assert.assertEquals(panelDto.getLongitude(), panel.getLongitude());
+		Assert.assertEquals(panelDto.getBrand(), panel.getBrand());
+		Assert.assertEquals(panelDto.getUnitOfMeasure(), panel.getUnitOfMeasure().toString());
 	}
 
 }
